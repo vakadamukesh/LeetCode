9514e6d2c4b317b5228537d0ecf8473b1baaf410
vakadamukesh <mukesh.wissenaire@gmail.com>
2024-07-19 15:11:24 +0530

    Time: 39 ms (97.54%), Space: 18.8 MB (58.78%) - LeetHub

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/1770-minimum-deletions-to-make-character-frequencies-unique.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minDeletions(string s) {
+         int freq[26] = {0};
+        
+        for(char &ch : s) {
+            freq[ch-'a']++;
+        }
+        sort(begin(freq), end(freq));
+        
+        int result = 0;
+        
+        for(int i = 24; i >= 0 && freq[i] > 0; i--) {
+            
+            if(freq[i] >= freq[i+1]) {
+                
+                int prev = freq[i];
+                
+                freq[i] = max(0, freq[i+1]-1);
+                
+                result  += (prev - freq[i]);
+                
+            }
+            
+        }
+          
+        return result;
+    }
+};
